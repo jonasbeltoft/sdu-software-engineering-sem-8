cfc37e7fcd148ecea25faa89e7a5b776d14e626d
Andreas <andreas.c.gade@gmail.com>
2024-05-10 00:07:04 +0200

    Extended functionality for sampling and changing batch size, and added heart beat every 10 sec

--- esp/main/app_main.c
+++ esp/main/app_main.c
@@ -40,6 +40,8 @@
 
 char TAG[UUID_SIZE];
 
+esp_mqtt_client_handle_t client;
+
 typedef struct 
 {
     int SAMPLE_RATE;
@@ -103,22 +105,44 @@ measurement_config parseCommand(const char *configJSON)
 
 // }
 
-void command_handler(esp_mqtt_client_handle_t client, int count)
+void command_handler(esp_mqtt_client_handle_t client, int SAMPLE_RATE, int BATCH_SIZE)
 {
-    TickType_t lastWakeTime = xTaskGetTickCount();
-    for (int i = 0; i < count; i++)
-    {
-        TickType_t timeSinceWake = lastWakeTime * portTICK_PERIOD_MS;
+
+    if (SAMPLE_RATE <= 0) {
+        ESP_LOGE(TAG, "Invalid sample rate. Please provide a positive integer.\n");
+        return;
+    }
+
+    float_t batch[BATCH_SIZE];
+
+    unsigned int interval_us = (unsigned int)(1.0 / SAMPLE_RATE * 1000000);
+
+    for (int i = 0; i < sizeof(batch)/sizeof(float_t); i++) {
         float_t measurement = sample();
         float_t roundedTemp = (float_t)roundf(measurement * 10) / 10;
-        printf("I have sampled: %f \n", measurement);
-        printf("Which is rounded to: %.1f \n", roundedTemp);
-        char reading[50];
+        printf("Made sample nr: %d with rounded temp: %.1f\n", i, roundedTemp);
 
-        // format is (readings left, temperature reading, time since launch)
-        sprintf(reading, "%d,%.1f,%lu", count - (i + 1), roundedTemp, timeSinceWake);
-        esp_mqtt_client_publish(client, CONFIG_MQTT_RESPONSE_TOPIC, reading, 0, 0, 0);
+        batch[i] = roundedTemp;
+
+        usleep(interval_us);
     }
+    printf("First and last sample are %.1f and %.1f\n", batch[0], batch[9]);
+
+    cJSON *json_array = cJSON_CreateArray();
+
+    for (int i = 0; i < sizeof(batch)/sizeof(float); i++) {
+        cJSON *json_float = cJSON_CreateNumber(round(batch[i] * 10) / 10.0);
+        cJSON_AddItemToArray(json_array, json_float);
+    }
+    char *json_string = cJSON_Print(json_array);
+
+    char mqttResp[1024];
+    sprintf(mqttResp, "%s: %s", TAG, json_string);
+
+    esp_mqtt_client_publish(client, "/device/log", mqttResp, 0, 0, 0);
+
+    cJSON_free(json_string);
+    cJSON_Delete(json_array);
 }
 
 /*
@@ -142,7 +166,7 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     {
     case MQTT_EVENT_CONNECTED:
         ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-        msg_id = esp_mqtt_client_publish(client, "/device/heartbeat", TAG, 0, 1, 1);
+        msg_id = esp_mqtt_client_publish(client, "/esp32/heartbeat", TAG, 0, 1, 1);
         ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
 
         msg_id = esp_mqtt_client_subscribe(client, CONFIG_MQTT_COMMAND_TOPIC, 2);
@@ -177,7 +201,7 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
         event->data[event->data_len] = 0;
         measurement_config config = parseCommand(event->data);
 
-        command_handler(client, config.SAMPLE_RATE);
+        command_handler(client, config.SAMPLE_RATE, config.BATCH_SIZE);
         break;
 
     case MQTT_EVENT_ERROR:
@@ -202,7 +226,7 @@ static void mqtt_app_start(void)
         .broker.address.uri = CONFIG_MQTT_BROKER,
     };
 
-    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    client = esp_mqtt_client_init(&mqtt_cfg);
     /* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
     esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
     esp_mqtt_client_start(client);    
@@ -213,6 +237,13 @@ void mqtt_task(void *pvParameters) {
     vTaskDelete(NULL);
 }
 
+void mqtt_heartbeat() {
+    while(1) {
+        esp_mqtt_client_publish(client, "/esp32/heartbeat", TAG, 0, 1, 1);
+        vTaskDelay(pdMS_TO_TICKS(10000));
+    }
+}
+
 void app_main(void)
 {
     ESP_ERROR_CHECK(nvs_flash_init());
@@ -221,8 +252,8 @@ void app_main(void)
     wifi_connection(CONFIG_WIFI_SSID, CONFIG_WIFI_PASSWORD);
 
     generate_uuid();
-    printf(TAG);
 
     
     xTaskCreate( mqtt_task, "MQTT Handler", 4096, NULL, 1, NULL);
+    xTaskCreate( mqtt_heartbeat, "MQTT Heartbeat", 4096, NULL, 1, NULL);
 }
